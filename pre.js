71a9f5da672756f9347b26bb119c601097fe8853
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-11 02:15:21 +1000

    Update pre.js

--- pre.js
+++ pre.js
@@ -3,11 +3,11 @@
     = br + '🖖 Hello World,'
     + br + br + 'My name is S.H.Y.A.M'
     + br + br + 'That's...'
-    + tab + bold('S')hyam.
-    + tab + bold('H')as.
-    + tab + bold('Y')our.
-    + tab + bold('A')nomaly.
-    + tab + bold('M')itigated.
+    + tab + bold('S') + 'hyam.'
+    + tab + bold('H') + 'as.'
+    + tab + bold('Y') + 'our.'
+    + tab + bold('A') + 'nomaly.'
+    + tab + bold('M') + 'itigated.'
     + br + br + shyam + 'means' + hyperlink('Dark-Complexioned Lord', 'https://en.wikipedia.org/wiki/List_of_titles_and_names_of_Krishna')
     + br + br + 'Here is how to pronounce Śyāma'
     + br + '<iframe width=\'200\' height=\'113\' src=\'https://www.youtube-nocookie.com/embed/CxU3JOWzZdg?rel=0&amp;controls=0&amp;showinfo=0\' frameborder=\'0\' allowfullscreen></iframe>'
@@ -17,3 +17,5 @@
     + br + br + 'AGENT S.'
     + br + br + '🕴'
     + br + br + bold('M') + 'igrating ' + bold('I') + 'nfestive ' + bold('B') + 'ugs ' + bold('!') + '!'
+
+; document.write(content) //////////////////////////////////////////////////////
