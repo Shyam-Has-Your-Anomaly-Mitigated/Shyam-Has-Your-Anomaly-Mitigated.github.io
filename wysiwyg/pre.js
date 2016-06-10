b9f63806831bd18d4ac8dcb737369f55b2a6d843
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-11 05:33:42 +1000

    Update pre.js

--- wysiwyg/pre.js
+++ wysiwyg/pre.js
@@ -15,4 +15,47 @@
     + input({type: 'button', value: '-'      , title: 'Small Text'      , onclick: '; format("-")'})
     + input({type: 'button', value: 'pt'     , title: 'Resize Text'     , onclick: '; format("pt")'})
     + input({type: 'button', value: 'H'      , title: 'Title Text'      , onclick: '; format("h")'})
+    + select(
+        /*
+            https://en.wikipedia.org/wiki/Web_typography#Generic_font_families
+            http://code.stephenmorley.org/html-and-css/the-myth-of-web-safe-fonts/
+            http://web.mit.edu/jmorzins/www/fonts.html
+            http://www.w3schools.com/cssref/css_websafe_fonts.asp
+        */
+        option('Web Safe', disabled: true)
+        // The monospaced stack
+        + option('Monospace', value: 'courier new,courier,monospace,sans-serif')
+        // The sans-monoed stack
+        + option('Sans-mono', value: 'lucida console,monaco,monospace,sans-serif')
+        // The pure-monoed stack
+        + option('Pure-mono', value: 'monospace,sans-serif', selected: true)
+        // The wide sans-serif stack
+        + option('Sans-serif', value: 'verdana,geneva,sans-serif')
+        // The narrow sans-serif stack
+        + option('Antenna', value: 'helvetica,arial,sans-serif') // Little Mermaid
+        // The wide serif stack
+        + option('Serif', value: 'georgia,utopia,charter,serif')
+        // The narrow serif stack
+        + option('Roman Num', value: 'times new roman,times,serif')
+        + option('!!!', disabled: true)
+        // The cursive stack
+        + option('Comical', value: 'comic sans ms,cursive,sans-serif')
+        // The fantasy stacks
+        + option('Serif', disabled: true)
+        + option('Georgia', value: 'georgia,serif')
+        + option('Palatino', value: 'palatino linotype,book antiqua,palatino,serif')
+        + option('Times', value: 'times new roman,times,serif')
+        + option('Sans-Serif', disabled: true)
+        + option('Arial', value: 'arial,helvetica,sans-serif')
+        + option('Arial Black', value: 'arial black,gadget,sans-serif')
+        + option('Impact', value: 'impact,charcoal,sans-serif')
+        + option('Lucida Dreama', value: 'lucida sans unicode,lucida grande,sans-serif')
+        + option('Tahoma', value: 'tahoma,geneva,sans-serif')
+        + option('Trebuchet MS', value: 'trebuchet ms,helvetica,sans-serif')
+        + option('Verdana', value: 'verdana,geneva,sans-serif')
+        , {onclick: '; format("f", this.value)', onchange: '; format("f", this.value)', title: 'Textual Font'}
+    )
+    + input({type: 'button', value: 'X'    , title: 'Clear Formatting', onclick: '; format("x")'})
+    + input({type: 'button', value: 'CSS'  , title: 'useCSS'          , onclick: '; format("css")'})
+    + input({type: 'button', value: 'Style', title: 'styleWithCSS'    , onclick: '; format("style")'})
 )
