97236c7b5c48809e5740984afaf6e47e3da210a3
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-11 02:25:28 +1000

    Update pre.js

--- pre.js
+++ pre.js
@@ -3,12 +3,12 @@
 ; document.write(
     br + '🖖 Hello World,'
     + br + br + 'My name is S.H.Y.A.M'
-    + br + br + 'That's...'
-    + tab + bold('S') + 'hyam.'
-    + tab + bold('H') + 'as.'
-    + tab + bold('Y') + 'our.'
-    + tab + bold('A') + 'nomaly.'
-    + tab + bold('M') + 'itigated.'
+    + br + br + 'That\'s...'
+    + br + tab + bold('S') + 'hyam.'
+    + br + tab + bold('H') + 'as.'
+    + br + tab + bold('Y') + 'our.'
+    + br + tab + bold('A') + 'nomaly.'
+    + br + tab + bold('M') + 'itigated.'
     + br + br + shyam + 'means' + hyperlink('Dark-Complexioned Lord', 'https://en.wikipedia.org/wiki/List_of_titles_and_names_of_Krishna')
     + br + br + 'Here is how to pronounce Śyāma'
     + br + '<iframe width=\'200\' height=\'113\' src=\'https://www.youtube-nocookie.com/embed/CxU3JOWzZdg?rel=0&amp;controls=0&amp;showinfo=0\' frameborder=\'0\' allowfullscreen></iframe>'
