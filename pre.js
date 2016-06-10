cbcb56bcf47c2099f8cbe187c03b4156dbe2b9bb
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-11 02:13:52 +1000

    Create pre.js

---
+++ pre.js
@@ -0,0 +1,19 @@
+; var shyam = 'श्याम'
+; var content
+    = br + '🖖 Hello World,'
+    + br + br + 'My name is S.H.Y.A.M'
+    + br + br + 'That's...'
+    + tab + bold('S')hyam.
+    + tab + bold('H')as.
+    + tab + bold('Y')our.
+    + tab + bold('A')nomaly.
+    + tab + bold('M')itigated.
+    + br + br + shyam + 'means' + hyperlink('Dark-Complexioned Lord', 'https://en.wikipedia.org/wiki/List_of_titles_and_names_of_Krishna')
+    + br + br + 'Here is how to pronounce Śyāma'
+    + br + '<iframe width=\'200\' height=\'113\' src=\'https://www.youtube-nocookie.com/embed/CxU3JOWzZdg?rel=0&amp;controls=0&amp;showinfo=0\' frameborder=\'0\' allowfullscreen></iframe>'
+    + br + br + 'I whisper sweet nothings to my machine and it just listens to me (like nobody else will); I am a ' + bold('Computer Whisperer') + ', but I whisper with my finger tips...'
+    + br + br + 'Contact: ' + hyperlink('shyam@shyam.id.au', 'mailto:shyam@shyam.id.au?Subject=YOU%20HAVE%20MAIL%20!!!')
+    + br + br + 'LIVE LONG AND PROSPER \V/,'
+    + br + br + 'AGENT S.'
+    + br + br + '🕴'
+    + br + br + bold('M') + 'igrating ' + bold('I') + 'nfestive ' + bold('B') + 'ugs ' + bold('!') + '!'
