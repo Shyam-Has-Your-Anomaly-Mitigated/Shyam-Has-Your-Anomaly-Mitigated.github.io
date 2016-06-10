64cf4b2194e1d8d5afaf62e40a8700b0638e1837
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-11 02:27:04 +1000

    Update pre.js

--- pre.js
+++ pre.js
@@ -14,7 +14,7 @@
     + br + br + 'I whisper sweet nothings to my machine and it just listens to me (like nobody else will); I am a ' + bold('Computer Whisperer') + ', but I whisper with my finger tips...'
     + br + br + 'Contact: ' + hyperlink('shyam@shyam.id.au', 'mailto:shyam@shyam.id.au?Subject=YOU%20HAVE%20MAIL%20!!!')
     + br + br + 'LIVE LONG AND PROSPER \V/,'
-    + br + br + 'AGENT S.'
-    + br + br + '🕴'
-    + br + br + bold('M') + 'igrating ' + bold('I') + 'nfestive ' + bold('B') + 'ugs ' + bold('!') + '!'
+    + br + 'AGENT S.'
+    + br + '🕴'
+    + br + bold('M') + 'igrating ' + bold('I') + 'nfestive ' + bold('B') + 'ugs ' + bold('!') + '!'
 )
