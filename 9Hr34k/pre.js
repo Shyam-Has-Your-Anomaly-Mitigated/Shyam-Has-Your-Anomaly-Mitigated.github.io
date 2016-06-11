e1211da2ef5b5b180f561b444ea9e0e90ac9c18e
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-12 03:50:22 +1000

    Update pre.js

--- 9Hr34k/pre.js
+++ 9Hr34k/pre.js
@@ -1,3 +1,4 @@
 ; document.write(
-    '9Hr34k is a challenge: write 34k bits/bytes/chars/lines/whatever in ≤9 hours (AKA the 1 day XP)'
+    br + heading('9Hr34k is a challenge', 3)
+    + 'Write 34k bits/bytes/chars/lines/whatever in ≤9 hours (AKA the 1 day XP)'
 )
