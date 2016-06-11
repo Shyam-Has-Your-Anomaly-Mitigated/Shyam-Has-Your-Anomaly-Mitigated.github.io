16eda96c36ffa4c3be6bea0d6d62b06c0feba218
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-12 02:31:54 +1000

    Update pre.js

--- warez/pre.js
+++ warez/pre.js
@@ -1,9 +1,11 @@
 ; document.write(
-    [
-        [ 'Program', 'Description', 'Source']
-        ,['GIMP'    , '2D Raster Editor', hyperlink('GNU Project', 'https://www.gimp.org/')]
-        ,['Inkscape', '2D Vector Editor', hyperlink('Indiana Jones', 'https://inkscape.org/')]
-        ,['Blender' , '3D Object Editor', hyperlink('Blender Foundation', 'https://www.blender.org/')]
-//        ,['', '', '']
-    ], true
+    ; tabulate(
+        [
+            [ 'Program', 'Description', 'Source']
+            ,['GIMP'    , '2D Raster Editor', hyperlink('GNU Project', 'https://www.gimp.org/')]
+            ,['Inkscape', '2D Vector Editor', hyperlink('Indiana Jones', 'https://inkscape.org/')]
+            ,['Blender' , '3D Object Editor', hyperlink('Blender Foundation', 'https://www.blender.org/')]
+//            ,['', '', '']
+        ], true
+    )
 )
