426c271d5d827d0c2643f91e7e55b9bb40837e85
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-12 05:03:32 +1000

    Update pre.js

--- mowgli/pre.js
+++ mowgli/pre.js
@@ -2,6 +2,9 @@
     br   + 'I am Kaa!'
     + br + '<iframe width=\'200\' height=\'113\' src=\'https://www.youtube-nocookie.com/embed/koUAZL8RUFU?rel=0&amp;controls=0&amp;showinfo=0\' frameborder=\'0\' allowfullscreen></iframe>'
     + br + '<iframe width=\'200\' height=\'113\' src=\'https://www.youtube-nocookie.com/embed/F1ILPl5FQaM?rel=0&amp;controls=0&amp;showinfo=0\' frameborder=\'0\' allowfullscreen></iframe>'
-    + br + br + 'Chronological orderings...'
+    + br + br + 'Chronological orderings'
     + br + 'Emails∈{"@ShYaM", "@rMIT"}'
+    + br + '@ShYaM = Blue Border'
+    + br + '@rMIT  = Red Border'
+    + br + 'URL navigation FTW! :D'
 )
