8243767444fdee66b69a846cf697837295403d1a
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-11 14:18:45 +1000

    Create pre.js

---
+++ bdfl/pre.js
@@ -0,0 +1,28 @@
+; document.write(
+    br   + format('Larry_Wall(Perl)', 'ub')
+    + br + 'I can use it to write everything else..!'
+    + br + 'Unre' + format('strict', 'bu') + 'ed is freedom; but it could be a lot more free/powerful..!'
+    + br + 'The author is not a co-founder of Google/'
+)
+; for(var i = 'A'.charCodeAt(0); i <= 'Z'.charCodeAt(0); i++) {
+    ; document.write(String.fromCharCode(i))
+}
+; document.write(
+    '.'
+    + br
+    + br + format('Bram_Moolenaar(Vim)', 'ub')
+    + br + 'ℝ341 programmers use the ' + hyperlink('cat', '/cat') + '!'
+    + br + 'But 1337 programmers use vim to clean up what the ' + hyperlink('cat', '/cat') + ' poops out.'
+    + br
+    + br + format('Linus_Torvalds(Linux_Kernel)', 'ub')
+    + br + 'A small part of what we think of as "Linux"...'
+    + br + 'Not so small these days..!'
+    + br
+    + br + format('Ton_Roosendaal(Blender)', 'ub')
+    + br + 'It has superluminal loading; relatively..!'
+    + br + 'This would be higher on this list if I actually knew ' + hyperlink('how to use it', 'http://catb.org/esr/writings/unix-koans/gui-programmer.html') + '...'
+    + br
+    + br + format('Guido_van_Rossum(Python)', 'ub')
+    + br + 'These days I use it as a calculator; I also use bc...'
+    + br + 'They\'re lucky perl doesn\'t have a fancy interactive interpreter; yet..' + hyperlink('!', 'https://en.wikipedia.org/wiki/Read%E2%80%93eval%E2%80%93print_loop')
+)
