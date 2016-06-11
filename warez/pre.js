8675f47200d4706f4421ff48b6edb47b7d158f3f
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-12 02:30:58 +1000

    Rename index.html to pre.js

---
+++ warez/pre.js
@@ -0,0 +1,9 @@
+; document.write(
+    [
+        [ 'Program', 'Description', 'Source']
+        ,['GIMP'    , '2D Raster Editor', hyperlink('GNU Project', 'https://www.gimp.org/')]
+        ,['Inkscape', '2D Vector Editor', hyperlink('Indiana Jones', 'https://inkscape.org/')]
+        ,['Blender' , '3D Object Editor', hyperlink('Blender Foundation', 'https://www.blender.org/')]
+//        ,['', '', '']
+    ], true
+)
