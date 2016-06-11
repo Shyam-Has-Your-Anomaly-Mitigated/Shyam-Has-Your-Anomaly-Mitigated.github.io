bd2a3d95c4495878009bb48f4f5f5c09ccc56b00
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-12 03:12:03 +1000

    Update pre.js

--- warez/pre.js
+++ warez/pre.js
@@ -5,13 +5,13 @@
                 'IMG', 'Program', 'Description', 'Source'
             ], [
                 image({url: 'https://www.gimp.org/images/wilber16.png', alt: 'ICO', class: 'ico'})
-                , 'GIMP'    , '2D Raster Editor', hyperlink('GNU Project', 'https://www.gimp.org/')
+                , 'GIMP', '2D Raster Editor', hyperlink('GNU Project', 'https://www.gimp.org/')
             ], [
                 image({url: 'https://media.inkscape.org/static/images/inkscape-logo.png', alt: 'ICO', class: 'ico'})
                 , 'Inkscape', '2D Vector Editor', hyperlink('Indiana Jones', 'https://inkscape.org/')
             ], [
                 image({url: 'https://www.blender.org/wp-content/themes/bthree/assets/images/favicon.ico', alt: 'ICO', class: 'ico'})
-                , 'Blender' , '3D Object Editor', hyperlink('Blender Foundation', 'https://www.blender.org/')]
+                , 'Blender', '3D ' + format('Object', 'u', {class: 'hover', title: 'Voxels'}) + ' Editor', hyperlink('Blender Foundation', 'https://www.blender.org/')]
         ], true
     )
 )
