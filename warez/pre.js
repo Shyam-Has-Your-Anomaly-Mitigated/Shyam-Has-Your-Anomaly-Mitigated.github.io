ec2e954bdbd6d3ff7ba66f3737e18e321ab37cfa
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-12 03:14:29 +1000

    Update pre.js

--- warez/pre.js
+++ warez/pre.js
@@ -11,7 +11,7 @@
                 , 'Inkscape', '2D Vector Editor', hyperlink('Indiana Jones', 'https://inkscape.org/')
             ], [
                 image({url: 'https://www.blender.org/wp-content/themes/bthree/assets/images/favicon.ico', alt: 'ICO', class: 'ico'})
-                , 'Blender', '3D ' + format('Object', 'u', {class: 'hover', title: 'Voxels'}) + ' Editor', hyperlink('Blender Foundation', 'https://www.blender.org/')]
+                , 'Blender', '3D Voxels Editor', hyperlink('Blender Foundation', 'https://www.blender.org/')]
         ], true
     )
 )
