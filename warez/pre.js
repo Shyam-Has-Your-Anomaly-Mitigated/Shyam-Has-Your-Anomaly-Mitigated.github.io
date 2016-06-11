098799317467f925d6dbc8047dd31ab588c2a5ac
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-12 03:04:37 +1000

    Update pre.js

--- warez/pre.js
+++ warez/pre.js
@@ -4,13 +4,13 @@
             [
                 'IMG', 'Program', 'Description', 'Source'
             ], [
-                image({url: 'https://www.gimp.org/images/wilber16.png', alt: 'ICO', class: 'ico', onerror: '; this.src="./favicon.ico"; this.onerror=""'})
+                image({url: 'https://www.gimp.org/images/wilber16.png', alt: 'ICO', class: 'ico'})
                 , 'GIMP'    , '2D Raster Editor', hyperlink('GNU Project', 'https://www.gimp.org/')
             ], [
-                image({url: 'https://media.inkscape.org/static/images/inkscape-logo.png', alt: 'ICO', class: 'ico', onerror: '; this.src="./favicon.ico"; this.onerror=""'})
+                image({url: 'https://media.inkscape.org/static/images/inkscape-logo.png', alt: 'ICO', class: 'ico'})
                 , 'Inkscape', '2D Vector Editor', hyperlink('Indiana Jones', 'https://inkscape.org/')
             ], [
-                image({url: 'https://www.blender.org/wp-content/themes/bthree/assets/images/favicon.ico', alt: 'ICO', class: 'ico', onerror: '; this.src="./favicon.ico"; this.onerror=""'})
+                image({url: 'https://www.blender.org/wp-content/themes/bthree/assets/images/favicon.ico', alt: 'ICO', class: 'ico'})
                 , 'Blender' , '3D Object Editor', hyperlink('Blender Foundation', 'https://www.blender.org/')]
         ], true
     )
