687226e644e64216cd996215db2cdc531efd3c34
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-12 01:12:25 +1000

    Update pre.js

--- bdfl/pre.js
+++ bdfl/pre.js
@@ -2,13 +2,13 @@
     br   + format('Larry_Wall(Perl)', 'ub')
     + br + 'I can use it to write everything else..!'
     + br + 'Unre' + format('strict', 'bu') + 'ed is freedom; but it could be a lot more free/powerful..!'
-    + br + 'The author is not a co-founder of ' + bold('G') + format('o', 'bu')
+    + br + 'The author is not a co-founder of ' + bold('G') + underline(bold('o'), {class: 'hover'})
 )
 ; for(var i = 'A'.charCodeAt(0); i <= 'Z'.charCodeAt(0); i++) {
     ; document.write(underline(String.fromCharCode(i), {class: 'hover'}))
 }
 ; document.write(
-    format('o', 'bu') + bold('gle') + '.'
+    underline(bold('o'), {class: 'hover'}) + bold('gle') + '.'
     + br
     + br + format('Bram_Moolenaar(Vim)', 'ub')
     + br + 'ℝ341 programmers use the ' + hyperlink('cat', '/cat') + '!'
