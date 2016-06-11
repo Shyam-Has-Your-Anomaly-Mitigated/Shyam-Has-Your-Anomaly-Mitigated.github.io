96e387112d129cbbdba98ccd9e4c296aecc4925e
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-11 12:54:16 +1000

    Create pre.js

---
+++ cat/pre.js
@@ -0,0 +1,57 @@
+; document.write(
+    br + 'A cat is <u class="hover" title="elegant">sleek</u>...'
+    + br + 'A cat is <u class="hover" title="graceful">slender</u>...'
+    + br + 'A cat is <u class="hover" title="agile">nimble</u>...'
+    + br + 'A cat is <u class="hover" title="alert">light-footed</u>...'
+    + br + 'A cat is <u class="hover" title="obfuscated">obscure</u>...'
+    + br + 'A cat is <u class="hover" title="steganographic">invisible</u>...'
+    + br + 'A cat is playful...'
+    + br + 'A cat can <u class="hover" title="Read Input \'(s)\'">see</u>...'
+    + br + 'A cat can <u class="hover" title="Write>Output">scratch</u>...'
+    + br + 'A cat can <u class="hover" title="Concatenate>Output Image TarBallZ">pounce</u>...'
+    + br + 'A cat can <u class="hover" title="HereSay <<WHEN">chase</u>...'
+    + br + 'A cat can <u class="hover" title="Pipe | DownUnder">poop</u>...'
+    + br + 'A cat has <u class="hover" title="Redirection>>Output<<<Input">whiskers</u>...'
+    + br
+    + br + 'What is a cats name? '
+    + br + italics('A cat has no name.')
+    + br + 'A cat is a lot of "-thing"s, but a cat has no name? '
+    + br + italics('A cat has no name!')
+    + br + 'If a cat has a name, a man will give a cat a graphical user interface? '
+    + br + italics('A cat has no name!!!')
+    + br
+    + br + 'Pussy dive into asian boxen...'
+    + br + 'Hypnotic ' + hyperlink('cirlces', 'http://www.sloganmaker.net/2014/10/trick-your-cat-with-circles/') + ' make a pussy ' + hyperlink('catatonic', 'https://youtu.be/XRciHfhLzrA') + '...'
+    + br + 'Japanese school girls eat pussy...'
+    + br
+    + br + 'Who are you? '
+    + br + italics('No ' + hyperlink('oNe', 'http://matrix.wikia.com/wiki/Prime_Program') + '.')
+    + br + 'If a cat says hiths name, a man will let hith roam in a virgin core tonight? '
+    + br + italics('A cat has no name.')
+    + br + 'If a cat says hiths name, a man will feed hith 9Hr34k-RAM tonight? '
+    + br + italics('A cat has no name.')
+    + br + 'If a cat says hiths name, a man will give hith a graphical user interface? '
+    + br + italics('A cat has no name.')
+    + br
+    + br + 'Kitty collisions pröduce entangled kitten...'
+    + br + 'Entangled kitten repröduce quantum para' + underline('die') + '...'
+    + br
+    + br + heading(underline('FROM CPT270 WEB PROGRAMMING FORUM'), 4)
+    + br + bold('Thread: ') + italics('Real (web) programmers use cat(s)')
+    + br
+    + br + underline('Shyam, 2014-12-02 19:38:50 EST')
+    + br + '<div><p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(255, 0, 0);">I\'m posting in "this forum so that other students don\'t panic".</span></span></p> <span style="font-family: courier new,courier,monospace,sans-serif;"><br></span> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(0, 51, 102);">Ever wondered why there\'s plenty of lolcatz lazing around in amongst the Internetz?</span></span></p> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(0, 51, 102);">Well, there\'s a fairly obvious reason why... (cat = independence)</span></span></p> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(0, 51, 102);">A cat is rather simple to handle, and has the most minimal of requirements.</span></span></p> <span style="font-family: courier new,courier,monospace,sans-serif;"><br></span> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(0, 51, 102);">Read the fourth panel, it\'s the only important one there. (clicking the image will hyperlink the source)</span></span></p> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><a title="https://xkcd.com/378/" href="http://www.explainxkcd.com/wiki/index.php/378" rel="nofollow"><img src="http://www.explainxkcd.com/wiki/images/0/0f/real_programmers.png" alt="https://xkcd.com/378/" title="https://xkcd.com/378/" height="406" width="740"></a></span></p> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(0, 51, 102);">NEWSFLASH: Microsoft only employs old-school-style coders! (See <a title="Microsoft only employs old-school coders." href="http://www.computerworld.com/article/2522170/app-development/microsoft-s-top-developers-prefer-old-school-coding-methods.html" rel="nofollow" id="FFS">here</a> for further details)</span></span></p></div>'
+    + br
+    + br + bold('Thread: ') + italics('Using notepad++')
+    + br
+    + br + underline('Kiet, 2014-12-17 12:32:03 EST')
+    + br + '<div><p class="default">I have now relized that using notepad++ is better than eclipse, and in fact, assigment 1 can be done all with notpad++. It\'s better as the title tag shows up as well. Do you thnk that learning in eclipse&nbsp;works out to be a&nbsp;handicap?</p></div>'
+    + underline('Trevor (Admin -\->), 2014-12-18 14:07:52 EST')
+    + br + '<div><p class="default">I have no comics ... any takers?</p></div>'
+    + underline('Shyam, 2014-12-18 13:19:02 EST')
+    + br + '<div><p><a target="_blank" title="https://xkcd.com/378/" href="http://www.explainxkcd.com/wiki/index.php/378" rel="nofollow"><img src="http://i.imgur.com/hIJDZil.png" alt="https://xkcd.com/378/" title="https://xkcd.com/378/" height="194" width="235"></a></p></div>'
+    + underline('Kiet, 2014-12-18 18:34:47 EST')
+    + br + '<div><p class="default">I don\'t get the last one?</p></div>'
+    + underline('Shyam, 2014-12-18 22:23:27 EST')
+    + br + '<div><p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(0, 51, 102);">Hello Kiet,</span></span></p> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(0, 51, 102);"><a href="https://en.wikipedia.org/wiki/Cat_%28Unix%29" rel="nofollow"><span style="color: rgb(0,51,102); text-decoration: underline"><strong style=";color: #036; background-color: #FFF">cat</strong></span></a> is a Unix console program with a minimal footprint.</span></span></p> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(0, 51, 102);">It\'s primary uses are; reading from files, combining files together into a single file, and writing to files.</span></span></p> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(0, 51, 102);">Utilising output redirection we can use it to write directly to file (as opposed to standard output).</span></span></p> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(0, 51, 102);">I like it because it\'s the most lightweight/fastest program used for coding.</span></span></p> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(128, 128, 128);">It also double functions as a steganographic tool by appending a compressed file to an image file, whereby the image file obscures the compressed file but still allows for both files to be accessed simultaneously.</span></span></p> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(128, 128, 128);">This works because image files keep all of their information in the file header, whilst compressed files keep all of their information in the file footer.</span></span></p> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(128, 128, 128);">So image files are read from the start of the file, and compressed files are read from the end of the file.</span></span></p> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(255, 0, 0);">cat is soooooooo useful! :D</span></span></p> <p><span style="font-family: courier new,courier,monospace,sans-serif;"><span style="color: rgb(255, 0, 0);">All other file readers/editors/writers and IDEs overcomplicate things.</span></span></p></div>'
+)
