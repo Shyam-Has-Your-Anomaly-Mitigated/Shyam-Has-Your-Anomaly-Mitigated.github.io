f5797fed55e1862e9ed120af4da49f316ec277a0
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-12 03:58:13 +1000

    Update pre.js

--- 9Hr34k/pre.js
+++ 9Hr34k/pre.js
@@ -1,4 +1,8 @@
 ; document.write(
-    heading(underline('9Hr34k is a ' + format('challenge', 'u', {class: 'hover', title: 'Should you choose to accept it.'}) + '!'), 4)
+    heading(
+        underline('9Hr34k is a ')
+        + format('challenge', 'u', {class: 'hover', title: 'Should you choose to accept it.'})
+        + underline('!')
+    ), 4)
     + 'Write 34K bits/bytes/chars/lines/whatever in ≤9HRs straight (AKA the 1 day XP)'
 )
