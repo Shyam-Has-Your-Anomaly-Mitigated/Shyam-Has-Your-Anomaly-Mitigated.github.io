dd11bc2c319e30b3e9020bfbc0c52f4d98307d7e
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-06-12 13:32:21 +1000

    Update pre.js

--- mowgli/pre.js
+++ mowgli/pre.js
@@ -1,5 +1,5 @@
 ; document.write(
-    br   + 'I am Kaa!'
+    heading(underline('I am Kaa!'), 4)
     + br + '<iframe width=\'200\' height=\'113\' src=\'https://www.youtube-nocookie.com/embed/koUAZL8RUFU?rel=0&amp;controls=0&amp;showinfo=0\' frameborder=\'0\' allowfullscreen></iframe>'
     + br + '<iframe width=\'200\' height=\'113\' src=\'https://www.youtube-nocookie.com/embed/F1ILPl5FQaM?rel=0&amp;controls=0&amp;showinfo=0\' frameborder=\'0\' allowfullscreen></iframe>'
     + br + br + 'Chronological orderings'
@@ -7,11 +7,11 @@
     + br + '@ShYaM = Blue Border'
     + br + '@rMIT  = Red Border'
     + br + 'URL navigation FTW! :D (no need for dynamic external loading; a simple branch.map in this cyberspace should do the hack)'
-    + heading('Site.Map Format', 4)
+    + heading(underline('Site.Map Format'), 4)
     + '@Me @ &lt;YYYY-MM-DD hh:mm:ss&gt;'
     + br + '@Me provides @Time zone information; both are unkown anyway...'
     + br + '@ShYaM has "+1000"; whatever that means...'
-    + heading('URL Format', 4)
+    + heading(underline('URL Format'), 4)
     + 'shyam.id.au/mowgli/N'
     + br + 'N as in the Nth in chronology'
 )
