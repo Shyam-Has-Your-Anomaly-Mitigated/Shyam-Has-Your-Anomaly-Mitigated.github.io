803c711e6e4eb23af5bf2f9711cdac7c4ebea252
Shyam <proprietor@shyam.id.au>
2016-07-13 02:56:00 +1000

    Update pre.js

--- tp/charcodes/pre.js
+++ tp/charcodes/pre.js
@@ -1,5 +1,5 @@
 ; document.write(//https://en.wikipedia.org/wiki/Category:Typography
-    br +
+    br
     + br + link(format('Others:', 'u'), '') + '∮,˜,∎,▮'
     + br + link(format('Proofs' , 'u'), '')
     + br + 'Therefore: ∴'
@@ -22,20 +22,18 @@
     + br + link(format('Equals:' , 'u'), 'https://en.wikipedia.org/wiki/Inequality_%28mathematics%29') + '≪,≫,<,>,≤,≥,=,≋,♎︎/≃/≈,~,≠,¬,.,=:,:=,:⇔,≜,≝,≐,≅,≡,⇔,↔'
     + br + link(format('MathBB:' , 'u'), 'https://en.wikipedia.org/wiki/Blackboard_bold') + 'ℕ,ℤ,ℚ,ℍ,ℂ,𝕆,𝕊,𝔸,ℝ,𝕄' // https://en.wikipedia.org/wiki/Template:Number_systems
     + br + link(format('Degree:' , 'u'), 'https://en.wikipedia.org/wiki/Degree_symbol') + '°'
-    + br +
-    + br +
+    + br
+    + br
     + br + link(format('Superscripts:' , 'u'), 'https://en.wikipedia.org/wiki/Unicode_subscripts_and_superscripts') + '⁰,ⁱ,²,³,⁴,⁵,⁶,⁷,⁸,⁹'
     + br + link(format('Subscripts &nbsp;:' , 'u'), 'https://en.wikipedia.org/wiki/Unicode_subscripts_and_superscripts') + '₀,₁,₂,₃,₄,₅,₆,₇,₈,₉'
     + br + link(format('Ordinals &nbsp;&nbsp;&nbsp;:' , 'u'), 'https://en.wikipedia.org/wiki/Ordinal_indicator') + 'ˢᵗ,ⁿᵈ,ʳᵈ,ᵗʰ'
     + br + link(format('Fractionals :' , 'u'), 'https://en.wikipedia.org/wiki/Fraction_%28mathematics%29#Typographical_variations') + '½,' + link('↉', 'https://en.wikipedia.org/wiki/Number_Forms') + ',⅓,⅔,¼,¾,⅕,⅖,⅗,⅘,⅙,⅚,⅛,⅜,⅝,⅞,⅐,⅑,⅒,⅟'
-    + br +
-    + br +
+    + br
+    + br
     + br + link(format('EmojiMarks:' , 'u'), 'https://en.wikipedia.org/wiki/Inverted_question_and_exclamation_marks') + '⸮,¿,?,‽,¡,!,.' //https://en.wikipedia.org/wiki/Content_clause#Interrogative_content_clauses
     + br + link(format('BoxedMarks:' , 'u'), 'https://en.wikipedia.org/wiki/Ballot_box#Unicode') + '■,□,☐,☑'
     + br + link(format('CheckMarks:' , 'u'), 'https://en.wikipedia.org/wiki/Check_mark') + '⍻,✓,✔,✅'
     + br + link(format('CrossMarks:' , 'u'), 'https://en.wikipedia.org/wiki/X_mark') + 'x,×,X,✕,☓,✖,✗,✘'
     + br + link(format('CycleMarks:' , 'u'), 'https://en.wikipedia.org/wiki/O_mark') + '◯,○,●'
     + br + link(format('FiredMarks:' , 'u'), 'https://en.wikipedia.org/wiki/Bullet_%28typography%29') + '‣,◦,∙'
-    + br +
-    + br +
 )
