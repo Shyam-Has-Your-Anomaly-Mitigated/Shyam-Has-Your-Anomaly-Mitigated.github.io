efd8071341b4d358ea49b616e27f38b3775aa86c
Shyam-Has-Your-Anomaly-Mitigated <proprietor@shyam.id.au>
2016-07-12 19:37:28 +1000

    GoT milk? :D

---
+++ milk/pre.js
@@ -0,0 +1,25 @@
+; document.write(
+    br + 'The Imp should wake the Spider with the line "wakey wakey, hand off snakey"...'
+    br + 'Yara wouldn\'t say that to Reek...'
+    br +
+    br + 'Seven Kingdoms for the Westerosi-lords in their respective Houses,'
+    br + bold('One Throne to rule them all, One Throne to find them,')
+    br + bold('One Throne to bring them all and in the winter bind them')
+    br + 'In the ℝeal North where the White Walkers wake...'
+    br +
+    br + br + bold(underline('Season 6'))
+    br +
+    br + 'Undead&nbsp;&nbsp;&nbsp;bastard king&nbsp;&nbsp;to the north...'
+    br + 'Foretold widow&nbsp;&nbsp;&nbsp;queen to the throne...'
+    br + 'Barren&nbsp;&nbsp;&nbsp;widow&nbsp;&nbsp;&nbsp;' + hyperlink('queen on a&nbsp;&nbsp;&nbsp;boat', 'https://youtu.be/avaSdC0QOUM') + '...'
+    br +
+    br + 'The ' + underline('chiefly-albino', {class: 'hover', title: 'She lacks albino-brows.'}) + ' heroine was like, "Wow, free boat ride for three armies!?! Now who should I take..? The Khalasar, the Unsullied, and...the Greyjoys."; the Second Sons have faced worse odds and won.'
+    br +
+    br + 'Star Trek could learn from this; 2D chess has 2 players, so it is only logical that 3D chess should have 3 players.'
+    br +
+    br + br + bold(underline('Season 7+'))
+    br +
+    br + 'No oNe will poach the Little Lion; she saw the play, she knows (' + italics(underline('roughly', {class: 'hover', title: 'The cub ate her sister (out).'})) + ') what happened...'
+    br +
+    br + 'The ' + underline('Daywalker', {class: 'hover', title: 'She lacks ginger-brows.'}) + ' is preggers with the Mad Dog\'s ' + underline('heir', {class: 'hover', title: 'The one true King in the North!'}) + '.'
+)
