c6fd8a3256007d2a4dbdf29310b7d74f9272e871
Shyam <proprietor@shyam.id.au>
2016-07-13 03:08:25 +1000

    Update pre.js

--- tp/charcodes/pre.js
+++ tp/charcodes/pre.js
@@ -1,10 +1,10 @@
 ; document.write(//https://en.wikipedia.org/wiki/Category:Typography
     br
-    + br + link(format('Others:', 'u'), '') + '∮,˜,∎,▮'
+    + br + link(format('Others:', 'u'), '') + ' ∮,˜,∎,▮'
     + br + link(format('Proofs' , 'u'), '')
     + br + 'Therefore: ∴'
     + br + 'Because &nbsp;: ∵'
-    + br + link(format('Counts' , 'u'), '') + '±,∓,+,−,(×,⋅,·),(÷,⁄),√,∑,Σ,∫,∞,∝,π'
+    + br + link(format('Counts:' , 'u'), '') + ' ±,∓,+,−,(×,⋅,·),(÷,⁄),√,∑,Σ,∫,∞,∝,π'
     + br + link(format('Groups' , 'u'), '')
     + br + 'e∈{S}'
     + br + 'e∉{S}'
@@ -19,21 +19,21 @@
     + br + '∃!x: P(x), means there is =1 x such that P(x) is true.'
     + br + '∃¬x: P(x), means there is <1 x such that P(x) is true.'
     + br + '{x∈ℝ|(∓∞)}, means x is in the set of ℝ, "such that" (∓∞) is true.'
-    + br + link(format('Equals:' , 'u'), 'https://en.wikipedia.org/wiki/Inequality_%28mathematics%29') + '≪,≫,<,>,≤,≥,=,≋,♎︎/≃/≈,~,≠,¬,.,=:,:=,:⇔,≜,≝,≐,≅,≡,⇔,↔'
-    + br + link(format('MathBB:' , 'u'), 'https://en.wikipedia.org/wiki/Blackboard_bold') + 'ℕ,ℤ,ℚ,ℍ,ℂ,𝕆,𝕊,𝔸,ℝ,𝕄' // https://en.wikipedia.org/wiki/Template:Number_systems
-    + br + link(format('Degree:' , 'u'), 'https://en.wikipedia.org/wiki/Degree_symbol') + '°'
+    + br + link(format('Equals:' , 'u'), 'https://en.wikipedia.org/wiki/Inequality_%28mathematics%29') + ' ≪,≫,<,>,≤,≥,=,≋,♎︎/≃/≈,~,≠,¬,.,=:,:=,:⇔,≜,≝,≐,≅,≡,⇔,↔'
+    + br + link(format('MathBB:' , 'u'), 'https://en.wikipedia.org/wiki/Blackboard_bold') + ' ℕ,ℤ,ℚ,ℍ,ℂ,𝕆,𝕊,𝔸,ℝ,𝕄' // https://en.wikipedia.org/wiki/Template:Number_systems
+    + br + link(format('Degree:' , 'u'), 'https://en.wikipedia.org/wiki/Degree_symbol') + ' °'
     + br
     + br
-    + br + link(format('Superscripts:' , 'u'), 'https://en.wikipedia.org/wiki/Unicode_subscripts_and_superscripts') + '⁰,ⁱ,²,³,⁴,⁵,⁶,⁷,⁸,⁹'
-    + br + link(format('Subscripts &nbsp;:' , 'u'), 'https://en.wikipedia.org/wiki/Unicode_subscripts_and_superscripts') + '₀,₁,₂,₃,₄,₅,₆,₇,₈,₉'
-    + br + link(format('Ordinals &nbsp;&nbsp;&nbsp;:' , 'u'), 'https://en.wikipedia.org/wiki/Ordinal_indicator') + 'ˢᵗ,ⁿᵈ,ʳᵈ,ᵗʰ'
-    + br + link(format('Fractionals :' , 'u'), 'https://en.wikipedia.org/wiki/Fraction_%28mathematics%29#Typographical_variations') + '½,' + link('↉', 'https://en.wikipedia.org/wiki/Number_Forms') + ',⅓,⅔,¼,¾,⅕,⅖,⅗,⅘,⅙,⅚,⅛,⅜,⅝,⅞,⅐,⅑,⅒,⅟'
+    + br + link(format('Superscripts:' , 'u'), 'https://en.wikipedia.org/wiki/Unicode_subscripts_and_superscripts') + ' ⁰,ⁱ,²,³,⁴,⁵,⁶,⁷,⁸,⁹'
+    + br + link(format('Subscripts &nbsp;:' , 'u'), 'https://en.wikipedia.org/wiki/Unicode_subscripts_and_superscripts') + ' ₀,₁,₂,₃,₄,₅,₆,₇,₈,₉'
+    + br + link(format('Ordinals &nbsp;&nbsp;&nbsp;:' , 'u'), 'https://en.wikipedia.org/wiki/Ordinal_indicator') + ' ˢᵗ,ⁿᵈ,ʳᵈ,ᵗʰ'
+    + br + link(format('Fractionals :' , 'u'), 'https://en.wikipedia.org/wiki/Fraction_%28mathematics%29#Typographical_variations') + ' ½,' + link('↉', 'https://en.wikipedia.org/wiki/Number_Forms') + ',⅓,⅔,¼,¾,⅕,⅖,⅗,⅘,⅙,⅚,⅛,⅜,⅝,⅞,⅐,⅑,⅒,⅟'
     + br
     + br
-    + br + link(format('EmojiMarks:' , 'u'), 'https://en.wikipedia.org/wiki/Inverted_question_and_exclamation_marks') + '⸮,¿,?,‽,¡,!,.' //https://en.wikipedia.org/wiki/Content_clause#Interrogative_content_clauses
-    + br + link(format('BoxedMarks:' , 'u'), 'https://en.wikipedia.org/wiki/Ballot_box#Unicode') + '■,□,☐,☑'
-    + br + link(format('CheckMarks:' , 'u'), 'https://en.wikipedia.org/wiki/Check_mark') + '⍻,✓,✔,✅'
-    + br + link(format('CrossMarks:' , 'u'), 'https://en.wikipedia.org/wiki/X_mark') + 'x,×,X,✕,☓,✖,✗,✘'
-    + br + link(format('CycleMarks:' , 'u'), 'https://en.wikipedia.org/wiki/O_mark') + '◯,○,●'
-    + br + link(format('FiredMarks:' , 'u'), 'https://en.wikipedia.org/wiki/Bullet_%28typography%29') + '‣,◦,∙'
+    + br + link(format('EmojiMarks:' , 'u'), 'https://en.wikipedia.org/wiki/Inverted_question_and_exclamation_marks') + ' ⸮,¿,?,‽,¡,!,.' //https://en.wikipedia.org/wiki/Content_clause#Interrogative_content_clauses
+    + br + link(format('BoxedMarks:' , 'u'), 'https://en.wikipedia.org/wiki/Ballot_box#Unicode') + ' ■,□,☐,☑'
+    + br + link(format('CheckMarks:' , 'u'), 'https://en.wikipedia.org/wiki/Check_mark') + ' ⍻,✓,✔,✅'
+    + br + link(format('CrossMarks:' , 'u'), 'https://en.wikipedia.org/wiki/X_mark') + ' x,×,X,✕,☓,✖,✗,✘'
+    + br + link(format('CycleMarks:' , 'u'), 'https://en.wikipedia.org/wiki/O_mark') + ' ◯,○,●'
+    + br + link(format('FiredMarks:' , 'u'), 'https://en.wikipedia.org/wiki/Bullet_%28typography%29') + ' ‣,◦,∙'
 )
