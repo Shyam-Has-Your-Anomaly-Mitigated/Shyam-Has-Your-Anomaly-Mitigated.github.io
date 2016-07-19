1967e12bbb4a1b1aa68163ae8fde3f21b7d1f81a
Shyam <shyam@shyam.id.au>
2016-07-19 20:13:29 +1000

    m

--- warez/git.sh/pre.js
+++ warez/git.sh/pre.js
@@ -2,7 +2,9 @@
         ; var xhr = new XMLHttpRequest()
         ; xhr.onreadystatechange = function() {
             ; if (xhr.readyState == 4 && xhr.status == 200) {
-                ; document.write(xhr.responseText.replace(/\n/g, br))
+                ; document.write(
+                    xhr.responseText.replace(/\n/g, br).replace(/</g, '&lt;')
+                )
             }
         }
         ; xhr.open("GET", url, true)
