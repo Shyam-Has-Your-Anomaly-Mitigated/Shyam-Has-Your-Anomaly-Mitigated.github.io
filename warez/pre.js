c8cf5706a17c98bc2da8046a7884a8d12d9a6dc2
Shyam <shyam@shyam.id.au>
2016-07-30 09:56:29 +1000

    This is probably more legal (less sources being stolen from); hopefully Google doesn't find out..!

--- warez/pre.js
+++ warez/pre.js
@@ -10,49 +10,49 @@
                 , 'th'
                 , {}
             ], [
-                [image({url: 'https://assets-cdn.github.com/favicon.ico', alt: 'ICO', c: 'ico'}), {}]
+                [image({url: 'https://www.google.com/s2/favicons?domain=www.github.com', alt: 'ICO', c: 'ico'}), {}]
                 , [link('CAT', '/cat', {class: 'hover', title: 'Ctrl∧Alt∧T=CLI; see after playful'}), {}]
                 , ['Bit Reader/Writer', {}]
                 , [link('UNIX', 'http://ftp.gnu.org/gnu/coreutils/'), {}]
                 , 'td'
                 , {}
             ], [
-                [image({url: 'http://www.vim.org/images/vim_shortcut.ico', alt: 'ICO', c: 'ico'}), {}]
+                [image({url: 'https://www.google.com/s2/favicons?domain=www.vim.org', alt: 'ICO', c: 'ico'}), {}]
                 , ['VIM', {}]
                 , ['Bit Editor', {}]
                 , [link('Indiana Jones', 'http://www.vim.org/'), {}]
                 , 'td'
                 , {}
             ], [
-                [image({url: 'https://git-scm.com/favicon.ico', alt: 'ICO', c: 'ico'}), {}]
+                [image({url: 'https://www.google.com/s2/favicons?domain=www.git-scm.com', alt: 'ICO', c: 'ico'}), {}]
                 , [link('GIT', 'git.sh'), {}]
                 , ['Software .Config Manager', {}]
                 , [link('L' + format('IN' + format('U', 'u') + 'X', 'b'), 'https://git-scm.com/'), {}]
                 , 'td'
                 , {}
             ], [// How did they implement their favicon?
-                [image({url: 'http://www.netfilter.org/images/netfilter-logo2.png', alt: 'ICO', c: 'ico'}), {}]
+                [image({url: 'https://www.google.com/s2/favicons?domain=www.netfilter.org', alt: 'ICO', c: 'ico'}), {}]
                 , [link('IPTables', 'iptables.sh'), {}]
                 , ['Firewall', {}]
                 , [link('Netfilter', 'http://www.netfilter.org/'), {}]
                 , 'td'
                 , {}
             ], [
-                [image({url: 'https://www.gimp.org/images/wilber16.png', alt: 'ICO', c: 'ico'}), {}]
+                [image({url: 'https://www.google.com/s2/favicons?domain=www.gimp.org', alt: 'ICO', c: 'ico'}), {}]
                 , ['GIMP', {}]
                 , ['2D Raster Editor', {}]
                 , [link('GNU Project', 'https://www.gimp.org/'), {}]
                 , 'td'
                 , {}
             ], [
-                [image({url: 'https://media.inkscape.org/static/images/inkscape-logo.png', alt: 'ICO', c: 'ico'}), {}]
+                [image({url: 'https://www.google.com/s2/favicons?domain=www.inkscape.org', alt: 'ICO', c: 'ico'}), {}]
                 , ['Inkscape', {}]
                 , ['2D Vector Editor', {}]
                 , [link('Indiana Jones', 'https://inkscape.org/'), {}]
                 , 'td'
                 , {}
             ], [
-                [[image({url: 'https://www.blender.org/wp-content/themes/bthree/assets/images/favicon.ico', alt: 'ICO', c: 'ico'})], {}]
+                [[image({url: 'https://www.google.com/s2/favicons?domain=www.blender.org', alt: 'ICO', c: 'ico'})], {}]
                 , ['Blender', {}]
                 , ['3D Voxels Editor', {}]
                 , [link('Blender Foundation', 'https://www.blender.org/'), {}]
