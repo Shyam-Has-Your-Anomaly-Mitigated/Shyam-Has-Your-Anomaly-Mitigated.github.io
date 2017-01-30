2a6562a0e36b8dfe49e83457d497ddfab1c0a255
Shyam <Shyam-Has-Your-Anomaly-Mitigated@users.noreply.github.com>
2017-01-30 22:51:13 +1100

    Update pre.js

--- cs/pre.js
+++ cs/pre.js
@@ -40,7 +40,7 @@
     + heading('Functional Compression', 4)
     +      'Mathematical functions can also be used to describe data in a compressed format...'
     + br + 'f(x)|a&lt;x&lt;b'
-    + br + 'Indexable.fun files and ' + link('filesystem format', 'https://en.wikipedia.org/wiki/List_of_Linux_distributions_that_run_from_RAM') + '.'
+    + br + br + 'Indexable.fun files and ' + link('filesystem format', 'https://en.wikipedia.org/wiki/List_of_Linux_distributions_that_run_from_RAM') + '.'
     + heading(format('Instructional Whispers', 'u'), 3)
     + heading('Logic: Vulcan vs ' + link('Jedi', 'https://en.wikipedia.org/wiki/Yoda_conditions'), 4)
     +      'Numerical logic must be ordered according to the number line; just as in the field of mathematics.'
