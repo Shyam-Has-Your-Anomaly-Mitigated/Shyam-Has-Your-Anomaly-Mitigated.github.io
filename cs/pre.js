e0d7be4ac87d8d6df80a665595786a934415b93b
Shyam <Shyam-Has-Your-Anomaly-Mitigated@users.noreply.github.com>
2017-07-22 16:34:57 +1000

    Update pre.js

--- cs/pre.js
+++ cs/pre.js
@@ -43,6 +43,16 @@
     +      'Mathematical functions can also be used to describe data in a compressed format...'
     + br + 'f(x)|a&lt;x&lt;b'
     + br + br + 'Indexable.fun files and ' + link('filesystem format', 'https://en.wikipedia.org/wiki/List_of_Linux_distributions_that_run_from_RAM') + '.'
+    + heading('Functional Network')
+    +      'shyam://address/parameterstring'
+    + br
+    + br + 'error_controlled(functionally_encrypted(functionally_compressed(transmissions))) by default; asymmetrical cryptography will be used to establish a shared secret in the form of a function, for functional security (functional compression provides the functions).'
+    + br + 'address is just a unique case-sensitive string that supports any encoding; with "invisible" whitespace, but "0b101_0011_1001 0x5_39" ≠ "0b101_0011_10010x5_39" = "0b101_0011_1001_0x5_39", even though "1337 1337" = "13371337", because literals should be spaced out'
+    + br + 'parameterstring has "visible" whitespace'
+    + br + 'underscores are whitespace'
+    + br + 'unencrypted simply takes the function f(x)=0, which is trivial to recognise'
+    + br + 'uncompressed is simply any file other than <name>.<extension>.fun, which is trivial to recognise'
+    + br + 'browsers should colourise the address bar green for secure connections, and red for insecure connections; SHYAM://* for uncompressed bandwidth hoggers, because bigger is bloated'
     + heading(format('Instructional Whispers', 'u'), 3)
     + heading('Logic: Vulcan vs ' + link('Jedi', 'https://en.wikipedia.org/wiki/Yoda_conditions'), 4)
     +      'Numerical logic must be ordered according to the number line; just as in the field of mathematics.'
