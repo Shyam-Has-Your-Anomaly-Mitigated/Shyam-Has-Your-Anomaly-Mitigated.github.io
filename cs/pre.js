dc49250f03554ba369e2e66cd2a9655ff1f2a151
Shyam <Shyam-Has-Your-Anomaly-Mitigated@users.noreply.github.com>
2017-12-29 22:21:23 +1100

    Update pre.js

--- cs/pre.js
+++ cs/pre.js
@@ -70,6 +70,7 @@
     + br + 'f(x)|a&lt;x&lt;b is different though; EG a wave of "data" between two points'
     + br + '^ those two points can be infinitesimally close to x (as integers); single value functions'
     + br + 'terms ∈ {xpᵉ, <s>xp, x</s>}, where p = nᵗʰ prime, e = power, x = constant; and just connect terms c̄ addition, subtraction, multiplication, division, ETC'
+    + br + '(a^b)^(c^d); is that a hyper operation?'
     + br + br + 'Indexable.fun files and ' + link('filesystem format', 'https://en.wikipedia.org/wiki/List_of_Linux_distributions_that_run_from_RAM') + '.'
     + heading('Functional Network', 4)
     +      'shyam:[/[/]]address/parameterstring'
