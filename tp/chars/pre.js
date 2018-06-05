2621530620e18644b0da44f010e298acf0477f31
Shyam <Shyam-Has-Your-Anomaly-Mitigated@users.noreply.github.com>
2018-06-06 07:14:43 +1000

    Update pre.js

--- tp/chars/pre.js
+++ tp/chars/pre.js
@@ -21,6 +21,7 @@ var
     + br + link(format('Proofs' , 'u'), '')
     + br + 'Therefore: ∴'
     + br + 'Because &nbsp;: ∵'
+    + br + '...: …'
     + br + link(format('Counts:' , 'u'), '') + ' ±,∓,+,−,(×,⋅,·),(÷,⁄),√,∛,∜,∑,Σ,∫,∞,∝,π'
     + br + link(format('Groups' , 'u'), '')
     + br + 'e∈{S}'
